4b5d317287c61ba62313707692f9da345bd25dd1
Mircho Rodozov <mrodozov@cern.ch>
2021-07-08 01:21:46 +0200

    add env var for datapkgs list

--- cmsswdata/cmsswdata.sh
+++ cmsswdata/cmsswdata.sh
@@ -14,7 +14,7 @@ cat << \EOF_TOOLFILE > ${toolfolder}/searchpath.xml
     <runtime name="CMSSW_DATA_PATH" value="$CMSSWDATA_BASE" type="path"/>
 EOF_TOOLFILE
 
-for toolbase in `echo %pkgreqs | tr ' ' '\n' | grep 'cms/data-'` ; do
+for toolbase in `echo ${CMSSWDATA_PKGREQUIRED} | tr ' ' '\n' | grep 'cms/data-'` ; do
   toolver=`basename $toolbase`
   pack=`echo $toolbase | cut -d/ -f2 | sed 's|data-||;s|-|/|'`
   echo "      <flags CMSSW_DATA_PACKAGE=\"$pack=$toolver\"/>" >> ${toolfolder}/cmsswdata.xml
