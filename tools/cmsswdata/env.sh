122429f71d46711ffb44df1ea658043800b0fc7c
Mircho Rodozov <mrodozov@cern.ch>
2021-07-09 21:38:11 +0200

    add runtime paths for cmsswdata

--- tools/cmsswdata/env.sh
+++ tools/cmsswdata/env.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -x
 
 cat << \EOF_TOOLFILE > ${SCRAM_TOOL_SOURCE_DIR}/cmsswdata.xml
 <tool name="cmsswdata" version="@TOOL_VERSION@">
@@ -12,14 +12,15 @@ cat << \EOF_TOOLFILE > ${SCRAM_TOOL_SOURCE_DIR}/searchpath.xml
     <runtime name="CMSSW_DATA_PATH" value="$CMSSWDATA_BASE" type="path"/>
 EOF_TOOLFILE
 
+cmssw_s_path=$(echo $TOOL_ROOT | sed "s|/cms/cmsswdata.*||g")
+
 for toolbase in `echo ${CMSSWDATA_PKGREQUIRED} | tr ' ' '\n' | grep 'cms/data-'` ; do
   toolver=`basename $toolbase`
   pack=`echo $toolbase | cut -d/ -f2 | sed 's|data-||;s|-|/|'`
   echo "      <flags CMSSW_DATA_PACKAGE=\"$pack=$toolver\"/>" >> ${SCRAM_TOOL_SOURCE_DIR}/cmsswdata.xml
-  echo "    <runtime name=\"CMSSW_SEARCH_PATH\" default=\"\" type=\"path\"/>" >> ${SCRAM_TOOL_SOURCE_DIR}/searchpath.xml
+  echo "    <runtime name=\"CMSSW_SEARCH_PATH\" default=\"${cmssw_s_path}/${toolbase}\" type=\"path\"/>" >> ${SCRAM_TOOL_SOURCE_DIR}/searchpath.xml
 done
 
 cat ${SCRAM_TOOL_SOURCE_DIR}/searchpath.xml >> ${SCRAM_TOOL_SOURCE_DIR}/cmsswdata.xml
 echo "  </tool>"      >> ${SCRAM_TOOL_SOURCE_DIR}/cmsswdata.xml
 rm -f ${SCRAM_TOOL_SOURCE_DIR}/searchpath.xml
-
