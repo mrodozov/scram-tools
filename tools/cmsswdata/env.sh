bcfada898ce133cedf8281cbe9f77e98436546b4
Mircho Rodozov <mrodozov@cern.ch>
2021-07-09 19:27:02 +0200

    fix worng subs

--- tools/cmsswdata/env.sh
+++ tools/cmsswdata/env.sh
@@ -13,8 +13,8 @@ cat << \EOF_TOOLFILE > ${SCRAM_TOOL_SOURCE_DIR}/searchpath.xml
 EOF_TOOLFILE
 
 for toolbase in `echo ${CMSSWDATA_PKGREQUIRED} | tr ' ' '\n' | grep 'cms/data-'` ; do
-  toolver=`basename $TOOL_ROOT`
-  pack=`echo $TOOL_ROOT | cut -d/ -f2 | sed 's|data-||;s|-|/|'`
+  toolver=`basename $toolbase`
+  pack=`echo $toolbase | cut -d/ -f2 | sed 's|data-||;s|-|/|'`
   echo "      <flags CMSSW_DATA_PACKAGE=\"$pack=$toolver\"/>" >> ${SCRAM_TOOL_SOURCE_DIR}/cmsswdata.xml
   echo "    <runtime name=\"CMSSW_SEARCH_PATH\" default=\"\" type=\"path\"/>" >> ${SCRAM_TOOL_SOURCE_DIR}/searchpath.xml
 done
